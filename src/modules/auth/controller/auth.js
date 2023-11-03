6c8607edfc62f05ea47d084e3241553388d3a4a1
M_twally <m.ametwally15@gmail.com>
2023-11-03 14:51:20 +0200

    add sign up feature

--- src/modules/auth/controller/auth.js
+++ src/modules/auth/controller/auth.js
@@ -1,2 +1,41 @@
-import connectDB from "../../../../DB/connection";
+import { StatusCodes } from "http-status-codes";
+import { ErrorClass } from "../../../utils/errorClass.js";
+import { asyncHandler } from "../../../utils/errorHandling.js";
+import { compare, hash } from "../../../utils/hashing.js";
+import { generateToken } from "../../../utils/generateAndVerifyToken.js";
+import { usermodel } from "../../../../DB/models/user.model.js";
+
+export const signUp = asyncHandler(async (req, res, next) => {
+    const { name, email, password, confirmpassword, age, phone, role, gender } = req.body;
+    console.log({ name, email, password, age, confirmpassword, phone, role, gender });
+    const existingUser = await usermodel.findOne({ where: { email: email } });
+
+    if (existingUser) {
+        return next (new ErrorClass( "Email already exists" ,StatusCodes.CONFLICT))
+    }
+
+    if (confirmpassword === password) {
+        const hashedPassword = hash(password);
+        const newUser = await usermodel.create({
+            name: name,
+            email: email,
+            password: hashedPassword,
+            age: age,
+            phone: phone,
+            role: role,
+            gender: gender,
+        });
+        const token = generateToken({
+            payload: {
+                name,
+                email,
+                password: newUser.password,
+            },
+        });
+
+        return res.status(StatusCodes.ACCEPTED).json({ message: "Done", result: newUser, token });
+    } else {
+        return next (new ErrorClass( "Password confirmation doesn't match" ,StatusCodes.BAD_REQUEST))
+    }
+});
 
