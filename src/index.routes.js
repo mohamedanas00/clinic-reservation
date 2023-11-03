c8f00edb71b14fde654802f8500c33d22506c322
M_twally <m.ametwally15@gmail.com>
2023-11-03 14:49:51 +0200

    add auth routes

--- src/index.routes.js
+++ src/index.routes.js
@@ -1,10 +1,14 @@
 import connectDB from "../DB/connection.js";
 import { globalErrorHandling } from "./utils/errorHandling.js";
+import AuthRouter from  "./modules/auth/auth.routes.js"
 
 const initApp = (app, express) => {
   app.use(express.json());
-  // app.use('/user', useRouter)
+  app.use('/auth', AuthRouter)
   app.use(globalErrorHandling);
+  app.use("/*", (req, res, next) => {
+    return res.json({ message: "in_valid routing" })
+})
   connectDB();
 };
 
