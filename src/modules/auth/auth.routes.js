b823c5d842340a45f1df0a7c00ab51c00777b029
M_twally <m.ametwally15@gmail.com>
2023-11-03 18:16:25 +0200

    adding validation middlleware to routes

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import *as AuthRouter from "./controller/auth.js"
+import {ValidationCoreFunction} from "../../middleware/validation.js"
+import * as Validators from "./auth.validations.js";
 const router=Router()
 
-router.post("/signup",AuthRouter.signUp)
-router.post("/signin",AuthRouter.signin)
+router.post("/signup",ValidationCoreFunction(Validators.signup),AuthRouter.signUp)
+router.post("/signin",ValidationCoreFunction(Validators.signin),AuthRouter.signin)
 
 
 
