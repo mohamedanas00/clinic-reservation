bd344138c96881360ef11da56c6590acea2da48d
M_twally <m.ametwally15@gmail.com>
2023-11-05 23:07:01 +0200

    user and slot routers validation

--- src/modules/slot/slot.routes.js
+++ src/modules/slot/slot.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import * as slotController from "./controller/slot.js";
 import auth, { userAuth } from "../../middleware/auth.js";
+import { ValidationCoreFunction } from "../../middleware/validation.js";
+import * as Validators from "./slot.validation.js";
 
 const slotRouter = Router();
 
-slotRouter.route('/').post(auth(userAuth.doctor), slotController.addSlot);
-slotRouter.route('/:slotId').patch(auth(userAuth.doctor), slotController.updateSlot);
+slotRouter.route('/').post(auth(userAuth.doctor),ValidationCoreFunction(Validators.addSlot), slotController.addSlot);
+slotRouter.route('/:slotId').patch(auth(userAuth.doctor),ValidationCoreFunction(Validators.updateSlot), slotController.updateSlot);
 
 export default slotRouter;
