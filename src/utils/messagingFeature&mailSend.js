380d4e5c7e35e38a73068c15acdc732c5d9842b2
mohamedanas00 <mohamed.anas7688@gmail.com>
2023-11-09 20:40:55 +0200

    help function send mail&createMessage

---
+++ src/utils/messagingFeature&mailSend.js
@@ -0,0 +1,24 @@
+import { StatusCodes } from "http-status-codes";
+import messageModel from "../../DB/models/message.model.js";
+import { ErrorClass } from "./errorClass.js";
+import { emailHtml, sendEmail } from "./email.js";
+
+export const notify = async ({patientId,doctorId,slotId,doctorName,doctorEmail,statusMessage,newSlot}={}) => {
+  try {
+    if(!newSlot){
+      var text = `Patient with ID:${patientId} is ${statusMessage} SlotId:${slotId}`;
+    }else{
+      var text = `Patient with ID:${patientId} is ${statusMessage} SlotId:${slotId} to slotId:${newSlot}`;
+    }
+    const html = emailHtml(text, doctorName);
+    sendEmail({ to: doctorEmail, subject: "Appointment Reservation", html });
+    await messageModel.create({
+      status: statusMessage,
+      userId: doctorId,
+      description: text,
+    });
+  } catch (error) {
+    console.log(error);
+    new ErrorClass(`Error for creating notify messaging`, StatusCodes.BAD_REQUEST)
+  }
+};
