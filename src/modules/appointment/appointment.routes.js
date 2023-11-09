17ba4831cc1ddfffbff55403906a678f5e607354
M_twally <m.ametwally15@gmail.com>
2023-11-10 01:54:17 +0200

    routes appointment validation

--- src/modules/appointment/appointment.routes.js
+++ src/modules/appointment/appointment.routes.js
@@ -1,18 +1,20 @@
 import { Router } from "express";
 import * as appointmentController from "./controller/appointment.js";
 import auth, { userAuth } from "../../middleware/auth.js";
+import { ValidationCoreFunction } from "../../middleware/validation.js";
+import * as Validators from "./appointment.validation.js";
 const router = Router();
 
 router.route('/:id')
-    .post(auth(userAuth.patient),appointmentController.addAppointment)
-    .put(auth(userAuth.patient),appointmentController.cancelAppointment)
+    .post(auth(userAuth.patient),ValidationCoreFunction(Validators.addAppointment),appointmentController.addAppointment)
+    .put(auth(userAuth.patient),ValidationCoreFunction(Validators.cancelAppointment),appointmentController.cancelAppointment)
 
 
 router.route('/:id/updateAppointment')
-    .put(auth(userAuth.patient),appointmentController.updateAppointment)
+    .put(auth(userAuth.patient),ValidationCoreFunction(Validators.updateAppointment),appointmentController.updateAppointment)
 
 
 router.route('/')
-    .get(auth(userAuth.patient),appointmentController.getAllAppointments)
+    .get(auth(userAuth.patient),ValidationCoreFunction(Validators.getAllAppointments),appointmentController.getAllAppointments)
 
 export default router;
