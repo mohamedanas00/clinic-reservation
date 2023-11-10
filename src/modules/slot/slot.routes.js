4b53f0f92ae3348f9829e6d573e8f6b0d53f7e0f
M_twally <m.ametwally15@gmail.com>
2023-11-10 02:48:10 +0200

    getallslotsvalidation

--- src/modules/slot/slot.routes.js
+++ src/modules/slot/slot.routes.js
@@ -8,7 +8,7 @@ const slotRouter = Router();
 
 slotRouter.route('/')
     .post(auth(userAuth.doctor),ValidationCoreFunction(Validators.addSlot), slotController.addSlot)
-    .get(auth(userAuth.doctor), slotController.getAllSlots);
+    .get(auth(userAuth.doctor),ValidationCoreFunction(Validators.getAllSlots), slotController.getAllSlots);
 
 slotRouter.route('/:slotId').patch(auth(userAuth.doctor),ValidationCoreFunction(Validators.updateSlotDate), slotController.updateSlotDate);
 // slotRouter.route('/:slotId').put(auth(userAuth.doctor), slotController.cancelSlot);
--- src/modules/slot/slot.validation.js
+++ src/modules/slot/slot.validation.js
@@ -6,19 +6,34 @@ export const addSlot = {
       date: joi.date().required(),
     }).required(),
   query: joi.object().keys({
-    
+
   }).required(),
-  params:joi.object().keys({
+  params: joi.object().keys({
 
   }).required()
 };
 
 export const updateSlotDate = {
-    body: joi.object().required()
-      .keys({date: joi.date().required(),}).required(),
-    query: joi.object().keys({}).required(),
-    params:joi.object().keys({
-      slotId:joi.number().required(),
-    }).required()
-  };
+  body: joi.object().required()
+    .keys({ date: joi.date().required(), }).required(),
+  query: joi.object().keys({}).required(),
+  params: joi.object().keys({
+    slotId: joi.number().required(),
+  }).required()
+};
+
+export const getAllSlots = {
+  body: joi.object().required()
+    .keys({
+
+    }).required(),
+  query: joi.object().keys({
+
+  }).required(),
+  params: joi.object().keys({
+
+  }).required()
+
+}
+
 
