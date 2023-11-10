a4f111384813d285fd70766f416b7c722a75f43c
mohamedanas00 <mohamed.anas7688@gmail.com>
2023-11-11 01:13:06 +0200

    add validation

--- src/modules/message/message.routes.js
+++ src/modules/message/message.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import * as messageController from "./controller/message.js";
 import auth, { userAuth } from "../../middleware/auth.js";
+import { ValidationCoreFunction } from "../../middleware/validation.js";
+import * as Validators from "./message.validation.js";
 const messageRouter = Router();
 
 messageRouter
   .route("/")
-  .get(auth(userAuth.doctor), messageController.getAllMessagingFeature);
+  .get(auth(userAuth.doctor), ValidationCoreFunction(Validators.getAllMessagingFeature),messageController.getAllMessagingFeature);
 
 export default messageRouter;
---
+++ src/modules/message/message.validation.js
@@ -0,0 +1,14 @@
+import joi from "joi";
+
+
+export const getAllMessagingFeature = {
+    body: joi.object().required()
+      .keys({
+      }).required(),
+    query: joi.object().keys({
+      
+      }).required(),
+    params:joi.object().keys({
+    }).required()
+  };
+
